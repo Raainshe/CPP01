a7c25a45470c307f1f814fb02e421b3f54e12a8b
Ryan Makoni <rmakoni@3-H-8.42heilbronn.de>
2025-04-09 12:51:45 +0200

    file structure and .hpp file added

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 11:58:21 by rmakoni           #+#    #+#             */
+/*   Updated: 2025/04/09 12:12:18 by rmakoni          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iomanip>
+# include <iostream>
+# include <string>
+
+class Zombie
+{
+  private:
+	std::string name;
+
+  public:
+	Zombie();
+};
+
+#endif
