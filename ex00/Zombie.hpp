670265d31453888514db0b2e239270b4b07afb23
Ryan Makoni <rmakoni@3-E-4.42heilbronn.de>
2025-04-11 12:22:19 +0200

    hpp changes

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 11:58:21 by rmakoni           #+#    #+#             */
-/*   Updated: 2025/04/09 12:12:18 by rmakoni          ###   ########.fr       */
+/*   Updated: 2025/04/09 12:54:41 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,16 @@ class Zombie
 	std::string name;
 
   public:
+	//constructors
 	Zombie();
+	Zombie(std::string name);
+	//destructors
+	~Zombie();
+	//mem functions
+	void announce(void);
 };
+
+Zombie	*newZombie(std::string name);
+void	randomChump(std::string name);
 
 #endif
