f7153e730a8272e0dc52e1e6f25702a75aa4062c
Raain <ryanbwgt@gmail.com>
2025-04-18 15:05:14 +0200

    ex04 completed

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 17:04:53 by rmakoni           #+#    #+#             */
-/*   Updated: 2025/04/17 19:15:48 by rmakoni          ###   ########.fr       */
+/*   Updated: 2025/04/18 14:52:52 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,41 @@
 //modify it immediatly and save it in a new file.
 //I need to read the file line by line and replace the s1 with s2.
 
-std::string readReplace(std::string filename, std::string s1, std::string s2)
+static bool readReplace(std::string filename, std::string s1, std::string s2)
 {
-    std::ifstream inputFile(filename);
-    
+    std::ifstream inputFile(filename.c_str());
+    if(!inputFile.is_open())
+    {
+        std::cerr << "Error: Could not open file " << filename << std::endl;
+        return (false);
+    }
+
+    std::string OutputFileName = "Replaced Data " + filename;
+    std::ofstream outputFile(OutputFileName.c_str());
+    if(!outputFile.is_open())
+    {
+        std::cerr << "Error: Could not create file " << OutputFileName << std::endl;
+        return (false);
+    }
+
+    std::string line;
+
+    while(std::getline(inputFile, line))
+    {
+        size_t ipos = 0;
+        while(ipos != std::string::npos)
+        {
+            ipos = line.find(s1, ipos);
+            if(ipos != std::string::npos)
+            {
+                line.erase(ipos, s1.length());
+                line.insert(ipos, s2);
+                ipos += s2.length();
+            }
+        }
+        outputFile << line << std::endl;
+    }
+    return true;
 }
 
 int main(int argc, char **argv)
@@ -35,6 +66,11 @@ int main(int argc, char **argv)
         return (1);
     }
     
-    std::string updatedFile = readReplace(argv[1], argv[2], argv[3]);
-
+    bool res = readReplace(argv[1], argv[2], argv[3]);
+    if(!res)
+    {
+        std::cerr << "Error: Could not read or write to file." << std::endl;
+        return (1);
+    }
+    std::cout << "File " << argv[1] << " has been modified and saved as Replaced Data " << argv[1] << std::endl;
 }
